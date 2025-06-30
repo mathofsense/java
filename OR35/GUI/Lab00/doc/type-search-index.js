4bc71bf997e05e8853aa69308578fcbd296c9533
Jiang Li <csdrli@users.noreply.github.com>
2025-06-29 21:38:35 -0400

    Day 6 cleanup

--- OR35/GUI/Lab00/doc/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"ClickablePanel"},{"p":"<Unnamed>","l":"ClickablePanel.Listener"},{"p":"<Unnamed>","l":"MyFirstGUI"}];updateSearchResults();
